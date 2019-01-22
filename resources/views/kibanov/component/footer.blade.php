835104dcda32ce8fc51dea94482d6586b1c73917
ordomalleus <ksiva_god_@bk.ru>
2019-01-22 17:33:22 +0500

    22.01.2019 - часть понятных исправлений 2

--- resources/views/kibanov/component/footer.blade.php
+++ resources/views/kibanov/component/footer.blade.php
@@ -8,32 +8,38 @@
             <div class="col-md-12">
                 <div class="address-container">
                     <div class="address-content">
-                        г.&nbsp;Пермь,&nbsp; ул.&nbsp;Советская,&nbsp;22
-                        <br/>+7 (342) 212 83 41
+                        <span>г.&nbsp;Пермь,&nbsp; ул.&nbsp;Советская,&nbsp;22</span>
+                        <br/>
+                        <span>+7 (342) 212 83 41</span>
                     </div>
                     <div class="address-content">
-                        г.&nbsp;Челябинск,&nbsp;пр.Ленина,&nbsp;77
-                        <br/>+7 (351) 265 44 73
+                        <span>г.&nbsp;Челябинск,&nbsp;пр.Ленина,&nbsp;77</span>
+                        <br/>
+                        <span>+7 (351) 265 44 73</span>
                     </div>
                     <div class="address-content">
-                        г.&nbsp;Санкт-Петербург,&nbsp;ул.&nbsp;Крылова,&nbsp;2
-                        <br/>+7 911 971 63 44
+                        <span>г.&nbsp;Санкт-Петербург,&nbsp;ул.&nbsp;Крылова,&nbsp;2</span>
+                        <br/>
+                        <span>+7 911 971 63 44</span>
                     </div>
                     <div class="address-content">
-                        г.&nbsp;Казань,&nbsp;ул.&nbsp;Карла&nbsp;Маркса,&nbsp;д.&nbsp;48
-                        <br/>+7 (987) 406-38-19
+                        <span>г.&nbsp;Казань,&nbsp;ул.&nbsp;Карла&nbsp;Маркса,&nbsp;д.&nbsp;48</span>
+                        <br/>
+                        <span>+7 (987) 406-38-19</span>
                     </div>
                     <div class="address-content">
                         г.&nbsp;Екатеринбург,&nbsp; ул.&nbsp;Мамина-Сибиряка,&nbsp;102
                         <br/>+7 (343) 355 23 08
                     </div>
                     <div class="address-content">
-                        г.&nbsp;Йошкар-Ола,&nbsp;ул.&nbsp;Комсомольская,&nbsp;130
-                        <br/>+7 (987) 704-97-27
+                        <span>г.&nbsp;Йошкар-Ола,&nbsp;ул.&nbsp;Комсомольская,&nbsp;130</span>
+                        <br/>
+                        <span>+7 (987) 704-97-27</span>
                     </div>
                     <div class="address-content">
-                        г.&nbsp;Краснодар,&nbsp; ул.&nbsp;Ленина,&nbsp;51
-                        <br/>+7 (918) 019-18-42
+                        <span>г.&nbsp;Краснодар,&nbsp; ул.&nbsp;Ленина,&nbsp;51</span>
+                        <br/>
+                        <span>+7 (918) 019-18-42</span>
                     </div>
                 </div>
             </div>
